817803bc9830375f04bde3f61a25c013643a7437
auscode <harshittext@gmail.com>
2024-01-29 11:33:00 +0530

    handling start

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const path = require("path");
 // Import the cors module
-const cors = require("cors"); 
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
